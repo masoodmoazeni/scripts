1682dc74608bb9dd496e8c87550fe047bc1c5d0b
masoodmoazeni <82110747+masoodmoazeni@users.noreply.github.com>
2022-10-05 13:14:17 +0330

    Create 2-kubernetes.sh
    install minikube kubectl and install tools require  for minikube

---
+++ 2-kubernetes.sh
@@ -0,0 +1,43 @@
+VERSION="1.0.1"
+
+#install minikube
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+cp minikube-linux-amd64 /usr/local/bin/minikube
+chmod 755 /usr/local/bin/minikube
+minikube version -o json
+
+#install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+mv ./kubectl /usr/local/bin/kubectl
+chmod 775 /usr/local/bin/kubectl
+kubectl version -o json
+
+
+apt-get install -y conntrack
+chmod 777 /tmp/juju-mkc8ab01ad3ea83211c505c81a7ee49a8e3ecb89
+sysctl fs.protected_regular=0
+
+#install cri-dockerd
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.2.0/cri-dockerd-v0.2.0-linux-amd64.tar.gz
+tar xvf cri-dockerd-v0.2.0-linux-amd64.tar.gz
+sudo mv ./cri-dockerd /usr/local/bin/ 
+cri-dockerd --help
+ wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+ wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+ sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+ sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+ systemctl daemon-reload
+ systemctl enable cri-docker.service
+ systemctl enable --now cri-docker.socket
+
+#install cri-tools
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.25.0/crictl-v1.25.0-linux-amd64.tar.gz
+sudo tar zxvf crictl-v1.25.0-linux-amd64.tar.gz -C /usr/local/bin
+
+echo "
+Install complete.
+"
+minikube version -o json
+kubectl version -o json
+
+minikube start --vm-driver=none
